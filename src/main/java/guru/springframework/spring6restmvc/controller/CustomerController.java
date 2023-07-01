1bb63db5f754e2432035522b24b9c1fd98a9a010
Christopher Hopkins <chrishopkins53@gmail.com>
2023-07-01 21:54:25 +0100

    Section 3456

---
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomerController.java
@@ -0,0 +1,70 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.model.Customer;
+import guru.springframework.spring6restmvc.services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("/api/v1/customer")
+@RequiredArgsConstructor
+@RestController
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @PatchMapping("{customerId}")
+    public ResponseEntity updateCustomerPatchById(@PathVariable("customerId") UUID customerId,
+                                                  @RequestBody Customer customer){
+
+        customerService.patchCustomerById(customerId, customer);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("{customerId}")
+    public ResponseEntity deleteById(@PathVariable("customerId") UUID customerId){
+
+        customerService.deleteById(customerId);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("{customerId}")
+    public ResponseEntity updateCustomerById(@PathVariable("customerId") UUID customerId,
+                                             @RequestBody Customer customer){
+
+        customerService.updateCustomerById(customerId, customer);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+
+    @PostMapping
+    //@RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity handlePost(@RequestBody Customer customer){
+
+        Customer savedCustomer = customerService.saveNewCUstomer(customer);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "api/v1/customer/" + savedCustomer.getId().toString());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Customer> listAllCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    @RequestMapping(value = "{customerId}", method = RequestMethod.GET)
+    public Customer getCustomerById(@PathVariable("customerId") UUID id){
+        return customerService.getCustomerById(id);
+    }
+
+}
