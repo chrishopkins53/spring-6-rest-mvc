812c045ddb6e96134dc0d08c647b1161669243d6
Christopher Hopkins <chrishopkins53@gmail.com>
2023-07-24 23:33:32 +0100

    Complete section 13

---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerCsvServiceImpl.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring6restmvc.services;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import guru.springframework.spring6restmvc.model.BeerCSVRecord;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class BeerCsvServiceImpl implements BeerCsvService {
+    @Override
+    public List<BeerCSVRecord> convertCSV(File csvFile) {
+        try {
+            List<BeerCSVRecord> beerCSVRecords = new CsvToBeanBuilder<BeerCSVRecord>(new FileReader(csvFile))
+                    .withType(BeerCSVRecord.class)
+                    .build().parse();
+            return beerCSVRecords;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/test/java/guru/springframework/spring6restmvc/bootstrap/BootStrapDataTest.java
+++ src/test/java/guru/springframework/spring6restmvc/bootstrap/BootStrapDataTest.java
@@ -2,15 +2,18 @@ package guru.springframework.spring6restmvc.bootstrap;
 
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
+import guru.springframework.spring6restmvc.services.BeerCsvService;
+import guru.springframework.spring6restmvc.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootStrapDataTest {
 
     @Autowired
@@ -19,17 +22,20 @@ class BootStrapDataTest {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    BeerCsvService csvService;
+
     BootStrapData bootStrapData;
 
     @BeforeEach
     void setUp() {
-        bootStrapData = new BootStrapData(beerRepository, customerRepository);
+        bootStrapData = new BootStrapData(beerRepository,customerRepository, csvService);
     }
     @Test
     void testRun() throws Exception {
         bootStrapData.run(null);
 
-        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(beerRepository.count()).isEqualTo(2413);
         assertThat(customerRepository.count()).isEqualTo(3);
     }
 }
